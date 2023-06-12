e77fa2b9d3cc746d9db38098780ccb1be3bad1a2
qduong42 <15814168+qduong42@users.noreply.github.com>
2023-06-12 19:17:59 +0200

    etc branch

---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Replace all occurrences of a string in a file by overwriting the file (i.e. in place):
+#	already done manually, keeping it in just in case
+# -R recursively change permissions for directories and their contents
+mkdir -p /var/www/html/wordpress;
+chmod -R 775 /var/www/html/wordpress;
+# The following example will change the owner of all files and
+# subdirectories in the /var/www/html/wordpress directory with a new owner and group named www-data :
+adduser -S -G www-data www-data;
+chown -R www-data:www-data /var/www/html/wordpress;
+mkdir -p /run/php/;
+touch /run/php/php-fpm81.pid;
+
+# After checking the requirements, download the wp-cli.phar file using wget or curl:
+# then, make it executable and move it to PATH
+# Downloads and extracts the main WordPress files
+if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+echo "Wordpress: setting up..."
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
+echo $(php wp-cli.phar --info --allow-root)
+chmod +x wp-cli.phar;
+mv wp-cli.phar /usr/local/bin/wp;
+cd /var/www/html/wordpress;
+
+
+#download and configure wordpress
+wp core download --allow-root;
+mv /var/www/wp-config.php /var/www/html/wordpress;
+echo "Wordpress: creating users..."
+# Creates WordPress tables in the database,
+# using the url, title and default admin user details provided
+#	--url=<url>
+# Address of the new site.
+#	--title=<site name>
+# The name of the new site.
+#	--admin_user=<username>
+# Administrator username.
+#	[--admin_password=<password>]
+# Password for the admin user. The default is a randomly generated string.
+#	--admin_email=<email>
+# Administrator's email address.
+wp core install --allow-root --url=127.0.0.1 --title=inception --admin_user=chief \
+				--admin_password=chief222 --admin_email=creator@42.fr;
+echo "Admin user has been created!"
+# Creates a new user
+#	<user-login>
+# Username to create.
+#	<user-email>
+# The email address of the user to create.
+#	[--role=<role>]
+# User role to create. Default: default role
+# Possible values ​​include "administrator", "editor", "author", "contributor", "subscriber".
+#	[--user_pass=<password>]
+# User password. Default: randomly generated
+wp user create --allow-root user user@42.fr --role=contributor --user_pass=user222;
+# Theme for WordPress
+# wp theme install inspiro --activate --allow-root
+
+
+echo "Wordpress: set up!"
+else
+echo "Wordpress: is already set up!"
+fi
+
+echo "Wordpress started on :9000"
+
+/usr/sbin/php-fpm81 -F
+
+# wp core install --allow-root --url=127.0.0.1 --title=inception --admin_user=chief --admin_password=chief222 --admin_email=creator@42.fr;
---
+++ try/wordpress/tools/wp-config.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'qduongdb' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'qduong42' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mariadb' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         ',2|~@}7!w|%B#x4*(!snC`*FOq3r70kI08AudTUM- 4X8<%f#]=+]+E8a]qUb8N/');
+define('SECURE_AUTH_KEY',  ']jHMvicj?_vED}S*+q%i=uLN5uQ=6jwCb/xuOQH{AR]19Pha=KBbP}=_zbAF^HWV');
+define('LOGGED_IN_KEY',    'qu<`M#WSm24QwC#;63>0RUZ#aO1s$d,2 (ymp?u>*a.iq@)Uo,+FEg2uDYoKbb@E');
+define('NONCE_KEY',        ')!UUrq# k%/2ZXY0-_D^ %-3j/<,C!PipRh{D*8r{Wvqu:zn+iB4&$yAgb:+kZB:');
+define('AUTH_SALT',        'iM-+{t+7FjTCh5_duJJyP}C>rKd@4LV|$f#qnv}gd%3{0i1N}$}mCI:=X$Yo,6Xe');
+define('SECURE_AUTH_SALT', '~1jMGh%Lw*fKwLpkl30$JwjL+DQbAX-v=tXK94>AIT}3!u87!u2;|CTn/;s%_|7/');
+define('LOGGED_IN_SALT',   'rrOnLjpdNQT[ S L&im29g_i*}AVDTa+!y3GM18_R~mzu^.17`}*1i|g4m;uB2wg');
+define('NONCE_SALT',       'O)L0?4W4j.eq+v>UmTkt-aB5&bmmS-HLj70:rs{P9[0Tac}C;aE|`0O43<^-mo.i');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+ /* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . 'var/www/html/wordpress' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
